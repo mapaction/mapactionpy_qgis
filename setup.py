70ef7052ac04d75637d43b1f8ca943d9448964fa
Andy Smith <andrewphilipsmith@users.noreply.github.com>
2020-04-17 10:45:22 +0100

    Bump version number (#6)
    * Update setup.py
    
    * Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import subprocess
 from setuptools import setup, find_packages
 from os import path, environ
 
-_base_version = '0.2.3'
+_base_version = '0.2.4'
 _root_dir = path.abspath(path.dirname(__file__))
 
 
